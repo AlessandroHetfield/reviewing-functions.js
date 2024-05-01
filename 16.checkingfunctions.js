2c12d36b010aad52efcab612cbf445b13e444714
AlessandroHetfield <160076296+AlessandroHetfield@users.noreply.github.com>
2024-05-01 17:51:18 -0400

    Add files via upload

---
+++ 16.checkingfunctions.js
@@ -0,0 +1,18 @@
+
+  // Reviewing functions SUBIDO A GITHUB
+
+  
+const yearsUntilRetirement = (birthYear, firstName) => {
+    const age = 2037 - birthYear;
+    const retirement = 65 - age ;
+    //return retirement;
+    return `${firstName} retires in ${retirement} years.`;
+}
+
+
+var dolphingrade1 = pe arseInt(prompt("Ingrese calificacion 1 de  Dolphins:"));
+var dolphingrade2 = parseInt(prompt("Ingrese calificacion 2 de  Dolphins:"));
+var dolphingrade3 = parseInt(prompt("Ingrese calificacion 3 de  Dolphins:"));
+var koalasgrade1 = parseInt(prompt("Ingrese calificacion 1 de  Koalas:"));
+var koalasgrade2 = parseInt(prompt("Ingrese calificacion 2 de  Koalas:"));
+var koalasgrade3 = parseInt(prompt("Ingrese calificacion 3 de  Koalas:"));
